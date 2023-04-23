bb6bd08854be5e4b6b9a88ac800ef6c544032e4d
Si Thu Win <sthuwin@gmail.com>
2023-04-23 10:46:03 +1000

    created datasource class which is Subject

---
+++ src/main/java/com/example/designPattern/observer/DataSource.java
@@ -0,0 +1,14 @@
+package com.example.designPattern.observer;
+
+public class DataSource {
+  public int getValue() {
+    return value;
+  }
+
+  public void setValue(int value) {
+    this.value = value;
+  }
+
+  private int value;
+
+}
