5f9700c54fcab5c5911f9f804a59e29b33986843
Si Thu Win <sthuwin@gmail.com>
2023-04-27 12:12:00 +1000

    refactor main class

--- src/main/java/com/example/designPattern/observer/trading/TradingMain.java
+++ src/main/java/com/example/designPattern/observer/trading/TradingMain.java
@@ -5,26 +5,16 @@ public class TradingMain {
     StatusBar statusBar = new StatusBar();
     StockListView stockListView = new StockListView();
 
+    Notification notification = new Notification();
+    notification.addObserver(statusBar);
+    notification.addObserver(stockListView);
+
+    StockService stockService1 = new StockService(notification);
+
     Stock stock1 = new Stock("stock1", 10);
-    Stock stock2 = new Stock("stock2", 20);
-    Stock stock3 = new Stock("stock3", 30);
-
-    statusBar.addStock(stock1);
-    statusBar.addStock(stock2);
-    statusBar.addStock(stock3);
-
-    stockListView.addStock(stock1);
-    stockListView.addStock(stock2);
-    stockListView.addStock(stock3);
-
-    statusBar.show();
-    stockListView.show();
+    stockService1.createStock(stock1);
 
     stock1.setPrice(11);
-    stock2.setPrice(21);
-    stock3.setPrice(31);
-
-    statusBar.show();
-    stockListView.show();
+    stockService1.priceChanged(stock1);
   }
 }
